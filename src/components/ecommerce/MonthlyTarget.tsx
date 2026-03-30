c0d883a2eeac84a3672e397ffeb1136d7cf31287
Moinul Islam <moinulilm10@gmail.com>
2026-03-30 13:52:37 +0600

    refactor: remove unused dashboard components and update favicon in index.html

--- src/components/ecommerce/MonthlyTarget.tsx
+++ src/components/ecommerce/MonthlyTarget.tsx
@@ -1,9 +1,5 @@
-import Chart from "react-apexcharts";
 import { ApexOptions } from "apexcharts";
 import { useState } from "react";
-import { Dropdown } from "../ui/dropdown/Dropdown";
-import { DropdownItem } from "../ui/dropdown/DropdownItem";
-import { MoreDotIcon } from "../../icons";
 
 export default function MonthlyTarget() {
   const series = [75.55];
@@ -66,7 +62,7 @@ export default function MonthlyTarget() {
   return (
     <div className="rounded-2xl border border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-white/[0.03]">
       <div className="px-5 pt-5 bg-white shadow-default rounded-2xl pb-11 dark:bg-gray-900 sm:px-6 sm:pt-6">
-        <div className="flex justify-between">
+        {/* <div className="flex justify-between">
           <div>
             <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">
               Monthly Target
@@ -98,8 +94,8 @@ export default function MonthlyTarget() {
               </DropdownItem>
             </Dropdown>
           </div>
-        </div>
-        <div className="relative ">
+        </div> */}
+        {/* <div className="relative ">
           <div className="max-h-[330px]" id="chartDarkStyle">
             <Chart
               options={options}
@@ -112,14 +108,14 @@ export default function MonthlyTarget() {
           <span className="absolute left-1/2 top-full -translate-x-1/2 -translate-y-[95%] rounded-full bg-success-50 px-3 py-1 text-xs font-medium text-success-600 dark:bg-success-500/15 dark:text-success-500">
             +10%
           </span>
-        </div>
-        <p className="mx-auto mt-10 w-full max-w-[380px] text-center text-sm text-gray-500 sm:text-base">
+        </div> */}
+        {/* <p className="mx-auto mt-10 w-full max-w-[380px] text-center text-sm text-gray-500 sm:text-base">
           You earn $3287 today, it's higher than last month. Keep up your good
           work!
-        </p>
+        </p> */}
       </div>
 
-      <div className="flex items-center justify-center gap-5 px-6 py-3.5 sm:gap-8 sm:py-5">
+      {/* <div className="flex items-center justify-center gap-5 px-6 py-3.5 sm:gap-8 sm:py-5">
         <div>
           <p className="mb-1 text-center text-gray-500 text-theme-xs dark:text-gray-400 sm:text-sm">
             Target
@@ -192,7 +188,7 @@ export default function MonthlyTarget() {
             </svg>
           </p>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,10 +1,8 @@
+import PageMeta from "../../components/common/PageMeta";
+import DemographicCard from "../../components/ecommerce/DemographicCard";
 import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
 import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
 import StatisticsChart from "../../components/ecommerce/StatisticsChart";
-import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
-import RecentOrders from "../../components/ecommerce/RecentOrders";
-import DemographicCard from "../../components/ecommerce/DemographicCard";
-import PageMeta from "../../components/common/PageMeta";
 
 export default function Home() {
   return (
@@ -20,20 +18,12 @@ export default function Home() {
           <MonthlySalesChart />
         </div>
 
-        <div className="col-span-12 xl:col-span-5">
-          <MonthlyTarget />
-        </div>
-
         <div className="col-span-12">
           <StatisticsChart />
         </div>
 
         <div className="col-span-12 xl:col-span-5">
           <DemographicCard />
-        </div>
-
-        <div className="col-span-12 xl:col-span-7">
-          <RecentOrders />
         </div>
       </div>
     </>
